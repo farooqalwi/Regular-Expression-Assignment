a0beaee929a2140d93bc14bc1b540730cfead6ba
Farooq Alwi <farooqalwi313@gmail.com>
2021-03-12 10:39:14 +0500

    working on email validation

--- src/regular/expression/assignment/RegularExpressionAssignment.java
+++ src/regular/expression/assignment/RegularExpressionAssignment.java
@@ -9,6 +9,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
 /**
  *
  * @author ALWI
@@ -20,6 +21,14 @@ public class RegularExpressionAssignment {
      */
     public static void main(String[] args) {
         
+        RE_for_integer_constant.integer_RE();
+        RE_for_email_validation.email_RE();
+        RE_for_url.url_RE();
+    }
+    
+    public static class RE_for_integer_constant {
+    static void integer_RE()
+    {
         Scanner sc = new Scanner(System.in); 
         System.out.print("Enter valid integer: ");
         
@@ -37,7 +46,52 @@ public class RegularExpressionAssignment {
         {
             System.out.println(input + " is an invlid integer.");
         }
-        
     }
+}
     
+    public static class RE_for_email_validation {
+    static void email_RE()
+    {
+        Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter valid email address: ");
+        
+        String input = sc.nextLine();
+        String regex = "^(.+)@(.+)$";
+        
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        
+        if (m.matches())
+        {
+            System.out.println(input + " is a valid email address.");
+        }
+        else
+        {
+            System.out.println(input + " is an invlid email address.");
+        }
+    }
+}
+    
+    public static class RE_for_url {
+        static void url_RE()
+        {
+            Scanner sc = new Scanner(System.in); 
+        System.out.print("Enter valid url: ");
+        
+        String input = sc.nextLine();
+        String regex = "";
+        
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(input);
+        
+        if (m.matches())
+        {
+            System.out.println(input + " is a valid url.");
+        }
+        else
+        {
+            System.out.println(input + " is an invlid url.");
+        }
+        }
+    }
 }
